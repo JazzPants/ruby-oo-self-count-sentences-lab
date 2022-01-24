468414dae69c5105eb45a08f61a3e1f62a887f4f
JazzPants <leighton.l14@gmail.com>
2022-01-25 09:45:58 +1100

    Done!

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,39 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if{|word| word.size<2}.count
+    #count works for array, not string
   end
 end
+
+puts "Hello, World!!!".count_sentences.inspect
+
+class Test
+  def bar=(val)
+  end
+  def bar
+    @bar
+  end
+  def baz=(val)
+    self.bar = val
+  end
+  def baz
+    self.bar
+  end
+end
+
+obj = Test.new
+obj.bar = 6
